aeba0dde628919423bdf6d411dba535fbbff573d
Yakipudding <butterandshoyu@gmail.com>
2018-12-03 22:06:49 +0900

    user_name set

--- drawNetworkGraph.py
+++ drawNetworkGraph.py
@@ -7,19 +7,20 @@ import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# 発言量が多い50人
-talks_count = collections.Counter(pd.read_csv('output/talk.csv', encoding='utf_8_sig')['talk_user']).most_common(50)
+# 発言量が多い順
+talks_count = collections.Counter(pd.read_csv('output/talk.csv', encoding='utf_8_sig')['talk_user']).most_common()
 df_mentions = pd.read_csv('output/mention.csv', encoding='utf_8_sig')
+df_users = pd.read_csv('output/users.csv', encoding='utf_8_sig', index_col=3) #index:user_id
 
 # ネットワーク図の作成
 ## ノードの生成：発言数で円を大きくする
 G = nx.Graph()
-G.add_nodes_from([(user, {"count":count}) for user, count in talks_count])
+G.add_nodes_from([(df_users.at[user,'profile_display_name_normalized'], {"count":count}) for user, count in talks_count])
 
 ## エッジの追加：メンション関係でエッジを追加する
 for index, mention in df_mentions.iterrows():
-    talk_user = mention['talk_user']
-    mention_user = mention['mention_user']
+    talk_user = df_users.at[mention['talk_user'],'profile_display_name_normalized']
+    mention_user = df_users.at[mention['mention_user'],'profile_display_name_normalized']
     if not G.has_node(talk_user) or not G.has_node(mention_user):
         continue
     if G.has_edge(talk_user, mention_user):
@@ -36,12 +37,12 @@ pos = nx.spring_layout(G, k=1.5)
 ## ノードの大きさを調整
 node_size = [ d['count']*50 for (n,d) in G.nodes(data=True)]
 ## ノードのスタイルを定義
-nx.draw_networkx_nodes(G, pos, node_color='b', alpha=0.2, node_size=node_size, font_weight="bold", font_family='VL Gothic')
-nx.draw_networkx_labels(G, pos, fontsize=14)
+nx.draw_networkx_nodes(G, pos, node_color='#f50057', alpha=0.3, node_size=node_size)
+nx.draw_networkx_labels(G, pos, fontsize=14, font_weight="bold", font_family='Yu Mincho')
 
 ## エッジの太さを調整
 edge_width = [ d['weight']*0.5 for (u,v,d) in G.edges(data=True)]
 ## エッジのスタイル
-nx.draw_networkx_edges(G, pos, alpha=0.4, edge_color='r', width=edge_width)
+nx.draw_networkx_edges(G, pos, alpha=0.4, edge_color='#f8bbd0', width=edge_width)
 
 plt.axis('off')
