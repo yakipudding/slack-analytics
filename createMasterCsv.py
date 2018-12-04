8b5c5b63c1b99b9626a07d554fdc94a5452ea7c7
Yakipudding <butterandshoyu@gmail.com>
2018-12-04 23:29:43 +0900

    display_name_custom追加（メンション用）

--- createMasterCsv.py
+++ createMasterCsv.py
@@ -9,4 +9,12 @@ for filename in files:
       d = json.loads(f.read())
 
   df = pd.io.json.json_normalize(d, sep='_')
+  if filename == 'users':
+    df['display_name_custom'] = ""
+    
+    for index, row in df.iterrows():
+        display_name_custom = row['name'] if row['profile_display_name_normalized'] == "" else row['profile_display_name_normalized']
+        print(display_name_custom)
+        df.at[index, 'display_name_custom'] = display_name_custom
+
   df.to_csv('output/' + filename +'.csv', encoding='utf_8_sig')
